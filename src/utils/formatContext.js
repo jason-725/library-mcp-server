0cbfd527f45ba865a07aa3d63bbbf191bdcee773
Jason Xiong <106554858+jason-725@users.noreply.github.com>
2025-11-08 13:22:22 -0600

    Add formatContext function to format book data

--- src/utils/formatContext.js
+++ src/utils/formatContext.js
@@ -1,1 +1,11 @@
+export function formatContext(books) {
+  return {
+    recommendations: books.map(b => ({
+      title: b.title,
+      author: b.author,
+      pages: b.pages,
+      year: b.first_publish_year,
+    })),
+  };
+}
 
