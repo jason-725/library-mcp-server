b8c241e9573a3ff71945a533eed262afd3773187
Jason Xiong <106554858+jason-725@users.noreply.github.com>
2025-11-08 13:22:02 -0600

    Add fetchBooks function to retrieve books from Open Library

--- src/services/openLibrary.js
+++ src/services/openLibrary.js
@@ -1,1 +1,14 @@
+import axios from "axios";
+
+export async function fetchBooks(query) {
+  const url = `https://openlibrary.org/search.json?q=${encodeURIComponent(query)}&limit=5`;
+
+  const { data } = await axios.get(url);
+  return data.docs.map(book => ({
+    title: book.title,
+    author: book.author_name?.[0],
+    pages: book.number_of_pages_median,
+    first_publish_year: book.first_publish_year,
+  }));
+}
 
